56d69996ac35d510f1c91ad1a8e75b6adf05ecdc
Donald Pakkies <donaldpakkies@gmail.com>
2018-04-06 09:40:12 +0200

    Feat: added a cli tool

---
+++ app/console/commands/serve.php
@@ -0,0 +1,66 @@
+<?php
+use Symfony\Component\Process\Process;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+
+class ServeCommand extends Command
+{
+  protected $commandName = 'serve';
+  protected $commandDescription = "Serve the application on the PHP development serve";
+
+  protected $commandArgumentPort = "port";
+  protected $commandArgumentDescription = "The port to serve the application on. <info>[default: 8000]</info>";
+
+  protected $commandOptionName = "port";
+  protected $commandOptionDescription = 'The port to serve the application on.';
+
+  protected function configure()
+  {
+    $this
+      ->setName($this->commandName)
+      ->setDescription($this->commandDescription)
+      ->addArgument(
+        $this->commandArgumentPort,
+        InputArgument::OPTIONAL,
+        $this->commandArgumentDescription
+      )
+      ->addOption(
+        $this->commandOptionName,
+        null,
+        InputOption::VALUE_NONE,
+        $this->commandOptionDescription
+      )
+    ;
+  }
+
+  protected function execute(InputInterface $input, OutputInterface $output)
+  {
+    $port = $input->getArgument($this->commandArgumentPort);
+
+    if ($port != null && is_numeric($port)) {
+      $output->writeln('['.date('D M d G:i:s Y').'] Listening on http://localhost:'.$port);
+      $output->writeln('Press Ctrl-C to quit.');
+      $cmd = passthru('cd public && php -S localhost:'.$port);
+      $output->writeln($cmd);
+    }
+    else if ($port == null) {
+      $output->writeln('['.date('D M d G:i:s Y').'] Listening on http://localhost:8000');
+      $output->writeln('Press Ctrl-C to quit.');
+      $cmd = passthru('cd public && php -S localhost:8000');
+      $output->writeln($cmd);
+    }
+    else if (!is_numeric($port)) {
+      $output->writeln('invalid port number');
+    }
+    else {
+      $output->writeln('['.date('D M d G:i:s Y').'] Listening on http://localhost:8000');
+      $output->writeln('Press Ctrl-C to quit.');
+      $cmd = passthru('cd public && php -S localhost:8000');
+      $output->writeln($cmd);
+    }
+  }
+}
