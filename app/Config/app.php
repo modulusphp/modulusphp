bcf78ee8f7a0ca7e10923961c3166b62e33122db
Donald Pakkies <donaldpakkies@gmail.com>
2018-04-15 19:19:39 +0200

    Feat: added a app file, this file should be used to specify auth controlllers

---
+++ app/Config/app.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'auth' => [
+
+        /**
+         * modulusPHP Auth Controllers
+         */
+        'controllers' => [
+            'Forgot',
+            'Login',
+            'Logout',
+            'Register',
+            'Reset'
+        ]
+    ]
+];
