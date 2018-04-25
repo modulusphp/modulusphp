4fe42373f18352a9a79bf86f3fab4d412a9a9d9b
Donald Pakkies <donaldpakkies@gmail.com>
2018-04-25 07:41:34 +0200

    Feat: updated the C Modulus Transcompiler

--- app/Core/cmodulus/Transcompiler.php
+++ app/Core/cmodulus/Transcompiler.php
@@ -5,41 +5,41 @@ class Transcompiler
   public function compile($script)
   {
     // func
-    $script = preg_replace('/func (.*?)\{/', "function $1 {", $script);
+    $script = preg_replace('/\bfunc\b (.*?)\{/', "function $1 {", $script);
 
     // echo
-    $script = preg_replace('/echo (.*?)\;/', "document.write($1)", $script);
+    $script = preg_replace('/\becho\b (.*?)\;/', "document.write($1)", $script);
     
     // println
-    $script = preg_replace('/println (.*?)\;/', "console.log($1);", $script);
+    $script = preg_replace('/\bprintln\b (.*?)\;/', "console.log($1);", $script);
 
     // error
-    $script = preg_replace('/error (.*?)\;/', "console.error($1);", $script);
+    $script = preg_replace('/\berror\b (.*?)\;/', "console.error($1);", $script);
 
     // if
-    $script = preg_replace('/if (.*?) \{/', "if ($1) {", $script);
+    $script = preg_replace('/\bif\b (.*?) \{/', "if ($1) {", $script);
     
     // while
-    $script = preg_replace('/while (.*?) \{/', "while ($1) {", $script);
+    $script = preg_replace('/\bwhile\b (.*?) \{/', "while ($1) {", $script);
 
     // for
-    $script = preg_replace('/for (.*?) \{/', "for ($1) {", $script);
+    $script = preg_replace('/\bfor\b (.*?) \{/', "for ($1) {", $script);
 
     // catch
-    $script = preg_replace('/catch (.*?) \{/', "catch ($1) {", $script);
+    $script = preg_replace('/\bcatch\b (.*?) \{/', "catch ($1) {", $script);
 
     /**
      * Variables
      */
 
     // @string
-    $script = preg_replace('/string\b (.*?)\;/', "var $1;", $script);
+    $script = preg_replace('/\bstring\b (.*?)\;/', "var $1;", $script);
 
     // @int
-    $script = preg_replace('/int\b (.*?)\;/', "var $1;", $script);
+    $script = preg_replace('/\bint\b (.*?)\;/', "var $1;", $script);
 
     // @bool
-    $script = preg_replace('/bool\b (.*?)\;/', "var $1;", $script);
+    $script = preg_replace('/\bbool\b (.*?)\;/', "var $1;", $script);
 
     return "<script>".$script."</script>";
   }
