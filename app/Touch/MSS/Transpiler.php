33f350e0218be6868e44c0275e22e6a025c3a94b
Donald Pakkies <donaldpakkies@gmail.com>
2018-05-12 13:55:29 +0200

    Feat: added a new transpiler for Modulus Style Sheet

---
+++ app/Touch/MSS/Transpiler.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Touch\MSS;
+
+use App\Touch\Transpiler as BaseTranspiler;
+
+class Transpiler implements BaseTranspiler
+{
+  public function compile($script)
+  {
+    
+  }
+}
