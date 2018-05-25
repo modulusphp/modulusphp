e39733d96fa4328dafd21ae7266083d3d97b3e72
Donald Pakkies <donaldpakkies@gmail.com>
2018-05-25 14:41:21 +0200

    Feat: updated the logger

--- app/Core/Debug.php
+++ app/Core/Debug.php
@@ -2,47 +2,49 @@
 
 namespace App\Core;
 
+use ModulusPHP\Framework\Core\Debugger;
+
 class Debug
 {
   /**
    * Info
-   * 
+   *
    * @param  string $text
    * @param  boolean $trace
    * @return output
-   */ 
+   */
   public static function info($text = null, $trace = false)
   {
-    Debug::output($text, $trace, '.INFO');
+    (new Debugger)->output($text, $trace, '.INFO');
   }
 
   /**
    * Error
-   * 
+   *
    * @param  string $text
    * @param  boolean $trace
    * @return output
    */
   public static function error($text = null, $trace = false)
   {
-    Debug::output($text, $trace, '.ERROR');
+    (new Debugger)->output($text, $trace, '.ERROR');
   }
 
   /**
    * Warning
-   * 
+   *
    * @param  string $text
    * @param  boolean $trace
    * @return output
    */
   public static function warning($text = null, $trace = false)
   {
-    Debug::output($text, $trace, '.WARNING');
+    (new Debugger)->output($text, $trace, '.WARNING');
   }
 
   /**
    * Debug
-   * 
+   *
    * @param  string $text
    * @return
    */
@@ -51,36 +53,5 @@ class Debug
     $logfile = 'storage/logs/modulus.log';
     $text = print_r($text, true);
     file_put_contents($logfile, $text.PHP_EOL, FILE_APPEND);
-  }
-
-  /**
-   * Output
-   * 
-   * @param  string $text
-   * @param  boolean $trace
-   * @param  string $type
-   * @return
-   */
-  private static function output($text = null, $trace = false, $type = '.INFO:')
-  {
-    /**
-     * ['file']
-     * ['line']
-     * ['function']
-     * ['args']
-     */
-    $logfile = 'storage/logs/modulus.log';
-
-    $currentdate = date("Y-m-d").' '.date("G:i:s");
-
-    $key = array_search(__FUNCTION__, array_column(debug_backtrace(), 'function'));
-    $file_trace = debug_backtrace()[$key]['file'];
-    $line_trace = debug_backtrace()[$key]['line'];
-
-    ($trace == true) ? $track_back = '['.$file_trace.'][line: '.$line_trace.']' : $track_back = '';
-
-    $text = print_r($text, true);
-
-    file_put_contents($logfile, $track_back.'['.$currentdate.'] '.config('app.env').$type.' '.$text.PHP_EOL, FILE_APPEND);
   }
 }
--- app/Core/Log.php
+++ app/Core/Log.php
@@ -2,49 +2,49 @@
 
 namespace App\Core;
 
-use Dotenv;
+use ModulusPHP\Framework\Core\Logger;
 
 class Log
 {
   /**
    * Info
-   * 
+   *
    * @param  string  $text
    * @param  boolean $trace
    * @return output
    */
   public static function info($text = null, $trace = false)
   {
-    Log::output($text, $trace, '.INFO');
+    (new Logger)->output($text, $trace, '.INFO');
   }
 
   /**
    * Error
-   * 
+   *
    * @param  string $text
    * @param  boolean $trace
    * @return output
    */
   public static function error($text = null, $trace = false)
   {
-    Log::output($text, $trace, '.ERROR');
+    (new Logger)->output($text, $trace, '.ERROR');
   }
 
   /**
    * Warning
-   * 
+   *
    * @param  string  $text
    * @param  boolean $trace
    * @return output
    */
   public static function warning($text = null, $trace = false)
   {
-    Log::output($text, $trace, '.WARNING');
+    (new Logger)->output($text, $trace, '.WARNING');
   }
 
   /**
    * Debug
-   * 
+   *
    * @param  string  $text
    * @return
    */
@@ -53,39 +53,5 @@ class Log
     $logfile = '../storage/logs/modulus.log';
     $text = print_r($text, true);
     file_put_contents($logfile, $text.PHP_EOL, FILE_APPEND);
-  }
-
-  /**
-   * Output
-   * 
-   * @param  string  $text
-   * @param  boolean $trace
-   * @param  string  $type
-   * @return
-   */
-  private static function output($text = null, $trace = false, $type = '.INFO:')
-  {
-    /**
-     * ['file']
-     * ['line']
-     * ['function']
-     * ['args']
-     */
-    $logfile = '../storage/logs/modulus.log';
-
-    $dotenv = new Dotenv\Dotenv(__DIR__.'/../../');
-    $dotenv->load();
-
-    $currentdate = date("Y-m-d").' '.date("G:i:s");
-
-    $key = array_search(__FUNCTION__, array_column(debug_backtrace(), 'function'));
-    $file_trace = debug_backtrace()[$key]['file'];
-    $line_trace = debug_backtrace()[$key]['line'];
-
-    ($trace == true) ? $track_back = '['.$file_trace.'][line: '.$line_trace.']' : $track_back = '';
-
-    $text = print_r($text, true);
-
-    file_put_contents($logfile, $track_back.'['.$currentdate.'] '.config('app.env').$type.' '.$text.PHP_EOL, FILE_APPEND);
   }
 }
