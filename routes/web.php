d8098e76fe40ae215f9afb44d8408586f44445ae
Donald Pakkies <donaldpakkies@gmail.com>
2018-05-25 14:23:51 +0200

    Feat: updated routes

--- routes/web.php
+++ routes/web.php
@@ -1,39 +1,45 @@
 <?php
 
-// HomeController
-Route::get('/', 'HomeController@index');
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application.
+|
+*/
 
-// TestController
-Route::group(['middleware' => 'dev', 'prefix' => 'test'], function() {
-  
-  Route::get('/cmodulus', 'TestController@cmodulus');
-  Route::get('/cmodulus/{name}', 'TestController@cmodulus');
-  Route::get('/for', 'TestController@for');
-  Route::get('/foreach', 'TestController@foreach');
-  Route::get('/if', 'TestController@if');
-  Route::get('/if/{name}', 'TestController@if');
-  Route::get('/while', 'TestController@while');
-  Route::get('/{name}/{age}', 'TestController@index');
-
-  // upload image from a post request
-  Route::post('/profilepic', 'TestController@storeProfilePic');
-  Route::post('/send', 'TestController@send');
-
+Route::get('/', function() {
+  view('welcome');
 });
 
-// Auth Controllers
-Route::group(['middleware' => 'guest', 'auth' => true], function() {
-
-  Route::get('/login', 'LoginController@index');
-  Route::post('/login', 'LoginController@index');
-
-  Route::get('/logout', 'LoginController@logout');
-
-  Route::get('/register', 'RegisterController@index');
-  Route::post('/register', 'RegisterController@store');
-
-  Route::get('/password/forgot', 'PasswordController@forgot');
-  Route::post('/password/forgot', 'PasswordController@forgot');
-  Route::get('/password/reset', 'PasswordController@reset');
-
-});
+/*
+|--------------------------------------------------------------------------
+| Auth Routes
+|--------------------------------------------------------------------------
+|
+| The following routes have been disabled. Feel free to enable them.
+|
+|--------------------------------------------------------------------------
+|
+| Route::group(['middleware' => 'auth', 'auth' => true], function() {
+|
+|    Route::get('/login', 'LoginController@index');
+|    Route::post('/login', 'LoginController@index');
+|
+|    Route::get('/logout', 'LoginController@logout');
+|
+|    Route::get('/register', 'RegisterController@index');
+|    Route::post('/register', 'RegisterController@store');
+|
+|    Route::get('/password/forgot', 'PasswordController@forgot');
+|    Route::post('/password/forgot', 'PasswordController@forgot');
+|
+|    Route::post('/password/reset', 'PasswordController@reset');
+|    Route::get('/password/reset', 'PasswordController@reset');
+|    Route::get('/password/reset/{token}', 'PasswordController@reset');
+|
+| });
+|
+|---------------------------------------------------------------------------
+*/
