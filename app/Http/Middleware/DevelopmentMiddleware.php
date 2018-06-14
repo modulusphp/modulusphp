c9a65140e7be313a893f7547db1cdc75dd543069
Donald Pakkies <donaldpakkies@gmail.com>
2018-06-14 17:06:09 +0200

    Refactor: updated the Dev Middleware

--- app/Http/Middleware/DevelopmentMiddleware.php
+++ app/Http/Middleware/DevelopmentMiddleware.php
@@ -3,13 +3,16 @@
 namespace App\Http\Middleware;
 
 use App\Core\Auth;
+use ModulusPHP\Http\Requests\Request;
 
 class DevelopmentMiddleware
 {
-  public function handle()
+  public function handle(Request $request)
   {
-    if (config('app.env') != 'local') {
-      redirect();
+    if (config('app.env') == 'local') {
+      return true;
     }
+
+    redirect();
   }
 }
