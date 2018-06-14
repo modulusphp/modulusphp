65983b91abee1b89a61d1f6aab2bb94be064e553
Donald Pakkies <donaldpakkies@gmail.com>
2018-06-14 17:08:00 +0200

    Refactor: removed auth secure

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,7 +6,6 @@ error_reporting(E_ALL & ~E_NOTICE);
 require_once __DIR__ . '/../vendor/autoload.php';
 
 try {
-  require_once __DIR__ . '/../app/Secure/auth.php';
   require_once __DIR__ . '/../app/Config/environment.php';
   require_once __DIR__ . '/../app/Config/database.php';
   require_once __DIR__ . '/../app/Core/App.php';
