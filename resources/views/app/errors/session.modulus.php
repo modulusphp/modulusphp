142d014768da7a539158fe878c7bce0d8501ffcd
Donald Pakkies <donaldpakkies@gmail.com>
2018-06-15 00:22:52 +0200

    Feat: added a sessions error view

---
+++ resources/views/app/errors/session.modulus.php
@@ -0,0 +1,17 @@
+{% partials('layouts.error') %}
+
+{% in('title') %}
+  Session has expired | modulusPHP
+{% endin %}
+
+{% in('main') %}
+
+  <div class="flex--center position--ref full--height">
+    <div class="__content">
+      <div class="__title __m-b-md">
+        Session has expired
+      </div>
+    </div>
+  </div>
+
+{% endin %}
