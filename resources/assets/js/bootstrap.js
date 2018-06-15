0e0c2479a28df3c9a8ef2a84ed9e2a43fade6564
Donald Pakkies <donaldpakkies@gmail.com>
2018-06-15 08:04:55 +0200

    Update bootstrap.js

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -21,7 +21,7 @@ try {
 
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
- * to our Laravel back-end. This library automatically handles sending the
+ * to our modulusPHP back-end. This library automatically handles sending the
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
