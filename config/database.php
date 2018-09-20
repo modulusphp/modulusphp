e9f9002b13e5ba5faa6334ce182e805407fdaeff
Donald Pakkies <donaldpakkies@gmail.com>
2018-09-20 07:03:11 +0200

    Feat: added database config file

---
+++ config/database.php
@@ -0,0 +1,46 @@
+<?php
+
+return [
+
+	/*
+  |--------------------------------------------------------------------------
+  | Default Database Connection Name
+  |--------------------------------------------------------------------------
+  |
+  | Here you may specify which of the database connections below you wish
+  | to use as your default connection for all database work. Of course
+  | you may use many connections at once using the Database library.
+  |
+  */
+
+	'default' => env('DB_CONNECTION', 'mysql'),
+
+	/*
+  |--------------------------------------------------------------------------
+  | Database Connections
+  |--------------------------------------------------------------------------
+  |
+  | Here are each of the database connections setup for your application.
+  | Of course, examples of configuring each database platform that is
+  | supported by Laravel is shown below to make development simple.
+  |
+  |
+  | All database work in modulusPHP is done through the PHP PDO facilities
+  | so make sure you have the driver for your particular database of
+  | choice installed on your machine before you begin development.
+  |
+  */
+
+	'connections' => [
+		'mysql' => [
+			'driver' => 'mysql',
+			'host' => env('DB_HOST', '127.0.0.1'),
+			'database' => env('DB_DATABASE', 'modulusphp'),
+			'username' => env('DB_USERNAME', 'modulusphp'),
+			'password' => env('DB_PASSWORD', ''),
+			'charset' => 'utf8',
+			'collation' => 'utf8_unicode_ci',
+			'prefix' => 'donaldpakkies_',
+		],
+	]
+];
