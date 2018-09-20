19af3553d0eb6835bff4853b2947af01f8e27be4
Donald Pakkies <donaldpakkies@gmail.com>
2018-09-20 07:04:27 +0200

    Feat: added a docsify config file

---
+++ config/docsify.php
@@ -0,0 +1,37 @@
+<?php
+
+return [
+
+  /*
+  |--------------------------------------------------------------------------
+  | Documentations config file
+  |--------------------------------------------------------------------------
+  |
+  |
+  */
+
+  'config' => [
+    'name' => 'modulusPHP',
+    'repo' => '',
+    'basePath' => '/docs',
+    'loadSidebar' => true,
+    'search' => [
+      'noData' => [
+        '/' => 'No results!'
+      ],
+      'paths' => 'auto',
+      'placeholder' => [
+        '/' => 'Search'
+      ]
+    ]
+  ],
+
+  'scripts' => [
+    '//unpkg.com/docsify/lib/docsify.min.js',
+    '//unpkg.com/docsify/lib/plugins/search.min.js'
+  ],
+
+  'sheets' => [
+    '//unpkg.com/docsify/lib/themes/vue.css'
+  ]
+];
