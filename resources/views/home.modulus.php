6030170d817921813f452f0dc7169a0dcb5b08d9
Donald Pakkies <donaldpakkies@gmail.com>
2018-10-01 19:22:12 +0200

    Feat: created a home page

---
+++ resources/views/home.modulus.php
@@ -0,0 +1,33 @@
+{% using('layouts.app') %}
+
+{% section('title') %}
+
+  Home
+
+{% endsection %}
+
+{% section('main') %}
+
+  <div class="flex--center position--ref full--height">
+    <div class="top-right links">
+      <a href="{{ url('/logout', true) }}"
+        onclick="event.preventDefault();
+        document.getElementById('logout-form').submit();">Logout</a>
+    </div>
+
+    <div class="__content __home_box">
+      <div class="row justify-content-center">
+        <div class="col-md-8">
+          <div class="card __shadow">
+            <div class="card-header __clean-header">Hi, {{ auth()->user()->name }}</div>
+
+            <div class="card-body">
+              You are logged in!
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+{% endsection %}
