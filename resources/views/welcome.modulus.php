68db6e928861dfad84ec07022fb908407f381fa2
Donald Pakkies <donaldpakkies@gmail.com>
2018-10-02 08:48:59 +0200

    Feat: updated the welcome ui

--- resources/views/welcome.modulus.php
+++ resources/views/welcome.modulus.php
@@ -29,6 +29,7 @@
       <div class="__info">
         <span>bootstrap: 4.1.3</span>
         <span>react.js: 16.4.2</span>
+        <span>vue.js: 2.5.16</span>
         <span>jquery: 3.3.1</span>
         <span>moment: 2.22.2</span>
       </div>
