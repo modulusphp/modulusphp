12751f6ea470fcf23964a929962821b0d0c89a46
Donald Pakkies <donaldpakkies@gmail.com>
2018-10-03 13:34:05 +0200

    Feat: checks if application is in maintenance mode or not

---
+++ app/Http/Middleware/CheckForMaintenanceMode.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Middleware;
+
+
+use Modulus\Http\Middleware\CheckForMaintenanceMode as Middleware;
+
+class CheckForMaintenanceMode extends Middleware
+{
+  /**
+   * The URIs that should be accessible while maintenance mode is enabled.
+   *
+   * @var array
+   */
+  protected $except = [
+    //
+  ];
+}
