4bae7ddb7c9263a42f95743e83f3f1c5a42a76c2
Donald Pakkies <donaldpakkies@gmail.com>
2018-10-03 13:36:07 +0200

    Feat: verifies csrf token
    Checks and verifies application csrf token

---
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Modulus\Http\Middleware\VerifyCsrfToken as Middleware;
+
+class VerifyCsrfToken extends Middleware
+{
+  /**
+   * The URIs that should be excluded from CSRF verification.
+   *
+   * @var array
+   */
+  protected $except = [
+    //
+  ];
+}
