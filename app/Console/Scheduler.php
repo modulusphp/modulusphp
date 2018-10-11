7231fb7bf9ac772477125e14fc0c0d2a8f590768
Donald Pakkies <donaldpakkies@gmail.com>
2018-10-11 23:17:47 +0200

    Feat: Added a schedular
    Schedule tasks

---
+++ app/Console/Scheduler.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Console;
+
+use Log;
+use GO\Scheduler as Schedule;
+use Modulus\Utility\Scheduler as Runner;
+
+class Scheduler extends Runner
+{
+  /**
+   * Start scheduler
+   *
+   * @param Scheduler $scheduler
+   * @return void
+   */
+  protected function schedule(Schedule $scheduler)
+  {
+    //
+  }
+}
