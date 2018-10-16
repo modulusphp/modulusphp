37918576b3dd11a6913fef4965968f9d8180499d
Donald Pakkies <donaldpakkies@gmail.com>
2018-10-16 22:50:37 +0200

    Feat: added a resolver for the router
    Responsible for loading routes

---
+++ app/Resolver/RouterResolver.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Resolver;
+
+use Modulus\Framework\Upstart\Service;
+
+class RouterResolver extends Service
+{
+  /**
+   * Register application routes
+   *
+   * @param object $app
+   * @return void
+   */
+  protected function boot($app) : void
+  {
+    $this->apiRoutes($app);
+    $this->webRoutes($app);
+  }
+
+  /**
+   * Load webRoutes
+   *
+   * @param object $app
+   * @return void
+   */
+  protected function webRoutes($app) : void
+  {
+    $app->route->make(base_path('routes/web.php'))
+         ->middleware('web')
+         ->register();
+  }
+
+  /**
+   * Load apiRoutes
+   *
+   * @param object $app
+   * @return void
+   */
+  protected function apiRoutes($app) : void
+  {
+    $app->route->make(base_path('routes/api.php'))
+         ->middleware('api')
+         ->prefix('api')
+         ->register();
+  }
+}
