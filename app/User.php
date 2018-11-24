076d9462ddfc6f09b91107f6b6d8ed5c3de1ebd6
Donald Pakkies <donaldpakkies@gmail.com>
2018-11-24 20:48:52 +0200

    Feat: cleaned up the user model

--- app/User.php
+++ app/User.php
@@ -2,26 +2,12 @@
 
 namespace App;
 
-use Modulus\Security\Hash;
-use Modulus\Framework\API;
 use Modulus\Hibernate\Model;
+use Modulus\Framework\API\User as API;
 
 class User extends API
 {
   use Model;
-
-  /*
-  |--------------------------------------------------------------------------
-  | User Model
-  |--------------------------------------------------------------------------
-  |
-  | The User Model extends the API class which extends Eloquent.
-  |
-  | The API class has the "generateSecret()" and "generateKey()" methods,
-  | these methods are responsible for assigning the secret and api key
-  | to a User.
-  |
-  */
 
   /**
    * The attributes that are mass assignable.
@@ -40,15 +26,4 @@ class User extends API
   protected $hidden = [
     'secret', 'email_verified_at', 'password', 'api_key', 'remember_token',
   ];
-
-  /**
-   * Encrypt the password using BCrypt
-   *
-   * @param string $value;
-   * @return string
-   */
-  public function setPasswordAttribute($value)
-  {
-    $this->attributes['password'] = Hash::make($value);
-  }
 }
