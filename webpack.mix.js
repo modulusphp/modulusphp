bf1fbaa059d5e37eb1b8a9e0b4262255d425ce98
Donald Pakkies <donaldpakkies@gmail.com>
2019-02-15 10:59:32 +0200

    Feat: upgrated npm packages and re-configured the webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,16 @@
-const {mix} = require('laravel-mix');
+const mix = require('laravel-mix');
 
-mix.setPublicPath('./public');
-mix.react('resources/js/app.js', './js')
-   .sass('resources/sass/app.scss', './css')
-   .version();
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Modulus application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.setPublicPath('./public')
+   .react('resources/js/app.js', './js')
+   .sass('resources/sass/app.scss', './css');
